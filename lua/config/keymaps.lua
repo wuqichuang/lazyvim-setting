95c0f50887a13812dfac8301c181b5276dba78fe
chuang <wuqichuang@gmail.com>
2024-08-25 11:53:56 +0800

    add markdown plugin and enable edgy change key for switch buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<M-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+vim.keymap.set("n", "<M-l>", "<cmd>bnext<cr>", { desc = "Prev Buffer" })
+
+vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
+vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
